d50896c669290d9171877cef89a4c879e9c8be5c
bitlottery <bit.lottery.admin@gmail.com>
2026-05-17 21:42:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: './',
     plugins: [
+      wasm(),
+      topLevelAwait(),
       react(), 
       tailwindcss(),
       nodePolyfills({
